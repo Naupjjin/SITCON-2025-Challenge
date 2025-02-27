087648f7d5b33084e0edce5a4466f296ff0180f8
naup <naup96721@gmail.com>
2025-02-27 09:37:09 +0800

    upd nsp

--- NeverStopPwning/public/share/chal.c
+++ NeverStopPwning/public/share/chal.c
@@ -2,6 +2,8 @@
 
 // gcc  chal.c -o chal -fno-stack-protector -no-pie --static
 int main(){
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
 	char buffer[8];
 	printf("Go!!!! ");
 	gets(buffer);
